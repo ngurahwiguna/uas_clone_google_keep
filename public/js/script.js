2bca35c1363d68e4675130bab21de5399b991383
Ngurah Wiguna <wayanngurahwiguna@gmail.com>
2026-07-11 15:03:16 +0800

    Style Google Keep home page

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,96 @@
-console.log("Google Keep Clone");
+let notes = [];
+
+const title = document.getElementById("title");
+const content = document.getElementById("content");
+const addBtn = document.getElementById("addBtn");
+const notesContainer = document.getElementById("notes");
+
+function renderNotes() {
+
+    notesContainer.innerHTML = "";
+
+    notes.forEach((note, index) => {
+
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+            <h3>${note.title}</h3>
+
+            <p>${note.content}</p>
+
+            <div class="actions">
+
+                <button onclick="editNote(${index})">
+                    Edit
+                </button>
+
+                <button onclick="deleteNote(${index})">
+                    Delete
+                </button>
+
+            </div>
+        `;
+
+        notesContainer.appendChild(card);
+
+    });
+
+}
+
+addBtn.addEventListener("click", () => {
+
+    if(title.value === "" || content.value === ""){
+
+        alert("Isi judul dan catatan!");
+
+        return;
+    }
+
+    notes.push({
+
+        title:title.value,
+
+        content:content.value
+
+    });
+
+    title.value = "";
+
+    content.value = "";
+
+    renderNotes();
+
+});
+
+function deleteNote(index){
+
+    notes.splice(index,1);
+
+    renderNotes();
+
+}
+
+function editNote(index){
+
+    const newTitle = prompt("Edit Judul", notes[index].title);
+
+    const newContent = prompt("Edit Isi", notes[index].content);
+
+    if(newTitle !== null){
+
+        notes[index].title = newTitle;
+
+    }
+
+    if(newContent !== null){
+
+        notes[index].content = newContent;
+
+    }
+
+    renderNotes();
+
+}
+
+renderNotes();
